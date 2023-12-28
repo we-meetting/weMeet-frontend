0b5299fe683cd4bd2611f38ab14c4b4acaa3832c
n ghtcjm <84369321+choi138@users.noreply.github.com>
2023-12-28 18:31:48 +0900

    feat: add text common component

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import { Text } from 'src/components';
+
+import * as S from './styled';
+
 export const MainPage: React.FC = () => {
   return (
-    <div>
-      <h1>위밋 메인 페이지</h1>
-    </div>
+    <S.MainPageContainer>
+      <Text size={3.4} weight="bold" style={{}}>
+        위밋 메인 페이지
+      </Text>
+    </S.MainPageContainer>
   );
 };
