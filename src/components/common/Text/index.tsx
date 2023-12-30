bcf45b4d18b15fa06fa0181e71e66c3119e9bdd1
n ghtcjm <84369321+choi138@users.noreply.github.com>
2023-12-30 17:37:25 +0900

    fix: add text Component descript

--- src/components/common/Text/index.tsx
+++ src/components/common/Text/index.tsx
@@ -12,6 +12,13 @@ export type TextProps = Partial<TextCommonProps> & {
   style?: React.CSSProperties;
 };
 
+/**\
+ * Text 컴포넌트
+ * @param {React.ReactNode} children - 텍스트
+ * @param {React.CSSProperties} style - 스타일
+ * @param {number} size - 폰트 사이즈
+ * @param {number} weight - 폰트 굵기
+ */
 export const Text: React.FC<TextProps> = ({ children, size = 1.1, weight = 400, style }) => {
   return (
     <S.TextElement size={size} weight={weight} style={style}>
