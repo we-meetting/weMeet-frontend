39634b005aa268fbf5db3cb5230feab0b3816851
MILGAM <93873127+Milgam06@users.noreply.github.com>
2024-01-15 04:45:28 +0900

    feat: add SEARCHBAR_CONTENT_LIST to partition Subject

---
+++ src/constants/searchbarContents.ts
@@ -0,0 +1,7 @@
+import { allSearchIcon, restaurantSearchIcon, enjoySearchIcon } from 'src/assets';
+
+export const SEARCHBAR_CONTENT_LIST: { text: string; image: string }[] = [
+  { text: '전체 검색', image: allSearchIcon },
+  { text: '맛집 검색', image: restaurantSearchIcon },
+  { text: '즐길 거리', image: enjoySearchIcon },
+];
