7e6bda72f147633bc9356e87a0870efb87d7e7f0
n ghtcjm <84369321+choi138@users.noreply.github.com>
2024-01-31 19:59:07 +0900

    fix: update index.ts

--- src/assets/foods/index.ts
+++ src/assets/foods/index.ts
@@ -3,6 +3,3 @@ export { default as Food2Svg } from './food2.svg';
 export { default as Food3Svg } from './food3.svg';
 export { default as Food4Svg } from './food4.svg';
 export { default as Food5Svg } from './food5.svg';
-export { default as Food6Svg } from './food6.svg';
-export { default as Food7Svg } from './food7.svg';
-export { default as Food8Svg } from './food8.svg';
